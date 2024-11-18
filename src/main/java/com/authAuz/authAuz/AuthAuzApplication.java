b755f8ba97b0869b111d618b2e8b849f2050a04b
bhubany <yadav.bhuban.by@gmail.com>
2024-11-18 20:42:00 +0545

    chore(): initial setup

---
+++ src/main/java/com/authAuz/authAuz/AuthAuzApplication.java
@@ -0,0 +1,13 @@
+package com.authAuz.authAuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAuzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authAuz/authAuz/AuthAuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authAuz.authAuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
