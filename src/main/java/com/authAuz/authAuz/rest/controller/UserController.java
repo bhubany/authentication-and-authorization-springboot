4a9a0fdeb63478d08fbd4426655b722f9da2ecab
bhubany <yadav.bhuban.by@gmail.com>
2024-12-31 19:41:46 +0545

    wip(): user controller and service

--- src/main/java/com/authAuz/authAuz/rest/controller/UserController.java
+++ src/main/java/com/authAuz/authAuz/rest/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.authAuz.authAuz.rest.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +21,34 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
     private final UserService userService;
 
+    @PostMapping("")
+    public ResponseEntity<Object> createUser() {
+        var users = userService.getUsers();
+        return ResponseEntity.ok(users);
+    }
+
     @GetMapping("")
     public ResponseEntity<Object> getUsers() {
         var users = userService.getUsers();
         return ResponseEntity.ok(users);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUser(@PathVariable UUID id) {
+        var users = userService.getUser(id);
+        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable UUID id) {
+        var users = userService.updateUser(id);
+        return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable UUID id) {
+        var users = userService.deleteUser(id);
+        return ResponseEntity.ok(users);
+    }
+
 }
--- src/main/java/com/authAuz/authAuz/service/UserService.java
+++ src/main/java/com/authAuz/authAuz/service/UserService.java
@@ -1,19 +1,29 @@
 package com.authAuz.authAuz.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    public void saveUser() {
-        System.out.println("Saving user");
+    public String createUser() {
+        return "User created successfully";
     }
 
     public String getUsers() {
         return "getUsers";
     }
 
-    public String getUser() {
-        return "getUser";
+    public String getUser(UUID id) {
+        return "user details with id: " + id;
+    }
+
+    public String updateUser(UUID id) {
+        return "user with id: " + id + " updated successfully";
+    }
+
+    public String deleteUser(UUID id) {
+        return "user with id: " + id + " deleted successfully";
     }
 
 }
